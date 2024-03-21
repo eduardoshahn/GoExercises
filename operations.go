3e89d6b7f1b8d13469c2fef19d63196b8de4963e
Eduardo S. Hahn <eduardohahn8111@gmail.com>
2024-03-21 17:09:47 -0300

    update

--- operations.go
+++ operations.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	receiveValues()
+	fmt.Println("\n")
+	showOneToTen()
+	fmt.Println("\n")
+	showSum()
+	fmt.Println("\n")
+	showPairs()
+	fmt.Println("\n")
+	fibonacciNumbers()
 }
 
 // Recebe o input do usuário e chama a função math() para trabalhar as variaveis a e b
@@ -29,4 +38,39 @@ func math(a, b int) {
 	restoDivisao := a % b
 	fmt.Printf("A soma dos dois números é: %d\nA subtração dos dois numeros é: %d\nA multiplicação dos dois numeros é: %d\nA divisão dos dois numeros é: %d\nO resto da divisão dos dois numeros é: %d\n", soma, subtracao, multiplicacao, divisao, restoDivisao)
 
+}
+
+func showOneToTen() {
+	for i := 1; i < 11; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Second / 5)
+	}
+}
+
+func showSum() {
+	soma := 0
+	for i := 1; i < 101; i++ {
+		soma += i
+	}
+	fmt.Println(soma)
+}
+
+func showPairs() {
+	for i := 1; i < 51; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func fibonacciNumbers() {
+	result := 0
+	var fibonacciList [19]int
+	fibonacciList[0] = 1
+	fibonacciList[1] = 1
+	for i := 0; i < 17; i++ {
+		result = fibonacciList[i] + fibonacciList[i + 1]
+		fibonacciList[i+2] = result
+	}
+	fmt.Println(fibonacciList)
 }
